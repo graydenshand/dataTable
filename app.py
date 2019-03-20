55aec78afa0a17ab54ac0895c7af16733745d35a
Grayden Shand <graydenshand@gmail.com>
2019-03-19 20:39:35 -0400

    submit queries via GET for linking tables;

--- app.py
+++ app.py
@@ -13,20 +13,17 @@ db_uri = os.environ.get("DATABASE_URI")
 @app.route('/', methods=["GET", "POST"])
 def index():
 	if request.method == "GET":
-		x = DataTable(db_uri)
-		sql = "SELECT customer_id, first_name FROM customer LIMIT 5;"
-		headers = ['Customer ID', 'First Name']
-		x.makeTable(sql, css_id="first_test_table", headers=headers, width=8)
-		return render_template('base.html', table1=x)
-	elif request.method == "POST":
-		sql = request.form.get('sql')
-		if request.form.get('headers') != '':
-			headers = request.form.get('headers').split(", ")
-		else:
-			headers = None
-		x = DataTable(db_uri)
-		x.makeTable(sql, css_id="first_test_table", headers=headers, width=8)
-		return render_template('base.html', table1=x)
+		if request.args.get('sql'):
+			sql = request.args.get('sql')
+			x = DataTable(db_uri)
+			x.makeTable(sql, css_id="first_test_table", width=8)
+			return render_template('base.html', table1=x)
+		else:	
+			x = DataTable(db_uri)
+			sql = "SELECT customer_id, first_name FROM customer LIMIT 5;"
+			headers = ['Customer ID', 'First Name']
+			x.makeTable(sql, css_id="first_test_table", headers=headers, width=8)
+			return render_template('base.html', table1=x)
 
 if __name__ == "__main__":
-	app.run(debug=True, )
+	app.run(debug=True, port=3000)
