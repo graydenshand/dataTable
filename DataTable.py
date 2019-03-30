030d4ceb2098817ee6a432193ca4f28d527f8652
Grayden Shand <graydenshand@gmail.com>
2019-03-29 21:32:48 -0400

    export csv

--- DataTable.py
+++ DataTable.py
@@ -1,4 +1,4 @@
-##Helper class for rendering data tables
+ ##Helper class for rendering data tables
 import psycopg2, psycopg2.extras
 import pymysql
 import json
@@ -16,28 +16,24 @@ class DataTable():
 		self.cur = self.conn.cursor(cursor_factory = psycopg2.extras.DictCursor)
 		self.data = []
 		self.css_id = ''
-		self.headers = None
 		self.sql = None
 		self.errors = None
 		self.width = 12
 		self.columns = []
 
 
-	def makeTable(self, sql, params=None, headers=None, css_id=None, width=12):
+	def makeTable(self, sql, params=None, css_id=None, width=12):
 		self.cur.execute(sql, params)
 		self.css_id = css_id
 		self.sql = self.cur.query
 		self.width = width
 		self.columns = [desc.name for desc in self.cur.description]
 		results = self.cur.fetchall()
-		self.headers=headers
 		self.data = []
-		if headers:
-			if len(headers) != len(results[0].keys()):
-				self.headers = None
 		for row in results:
 			line = OrderedDict()
 			for i, k in enumerate(self.columns):
 				line[k] = str(row.values()[i]).decode('utf-8')
 			self.data.append(line)
+		return results
 
--- app.py
+++ app.py
@@ -1,31 +1,56 @@
 import psycopg2, psycopg2.extras
 import pymysql
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect, send_file
 from collections import OrderedDict 
 import os
 import urllib
 from DataTable import DataTable
+import csv
 
 app = Flask(__name__)
 
 db_uri = os.environ.get("DATABASE_URI")
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/', methods=["GET"])
 def index():
-	if request.method == "GET":
-		if request.args.get('sql'):
-			sql = request.args.get('sql')
-			sql = urllib.unquote(sql)
-			x = DataTable(db_uri)
-			x.makeTable(sql, css_id="first_test_table", width=8)
-			return render_template('base.html', table1=x)
-		else:	
-			x = DataTable(db_uri)
-			sql = "SELECT customer_id, first_name FROM customer LIMIT 5;"
-			headers = ['Customer ID', 'First Name']
-			x.makeTable(sql, css_id="first_test_table", headers=headers, width=8)
-			return render_template('base.html', table1=x)
+	if request.args.get('sql'):
+		sql = request.args.get('sql')
+		sql = urllib.unquote(sql)
+		x = DataTable(db_uri)
+		x.makeTable(sql, css_id="first_test_table", width=8)
+		return render_template('base.html', table1=x, sql=sql)
+	else:	
+		x = DataTable(db_uri)
+		sql = "SELECT customer_id, first_name FROM customer LIMIT 5;"
+		x.makeTable(sql, css_id="first_test_table", width=8)
+		return render_template('base.html', table1=x, sql=sql)
+
+@app.route('/download_csv', methods=['GET'])
+def download_csv():
+	if request.args.get('sql'):
+		sql = request.args.get('sql')
+		sql = urllib.unquote(sql)
+		x = DataTable(db_uri)
+		results = x.makeTable(sql)
+		headers = x.columns
+		f = open('auto_increment.txt', 'r')
+		auto_increment = int(f.read()) + 1
+		print(auto_increment)
+		f.close()
+		f = open('auto_increment.txt', 'w')
+		f.write(str(auto_increment))
+		f.close()
+		fn = 'static/output_{}.csv'.format(auto_increment)
+		with open(fn, 'w') as f:
+			writer = csv.writer(f)
+			writer.writerow(headers)
+			for i, row in enumerate(results):
+				writer.writerow(row)
+		return send_file(fn,
+                     mimetype='text/csv',
+                     attachment_filename='output.csv',
+                     as_attachment=True)
 
 if __name__ == "__main__":
 	app.run(debug=True, port=3000)
