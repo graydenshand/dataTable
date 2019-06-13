04c6d8cf5e3b79a6044ac0fc183854d56a7701a1
Grayden Shand <graydenshand@gmail.com>
2019-06-13 09:42:22 -0400

    login page

--- app.py
+++ app.py
@@ -1,33 +1,50 @@
 import psycopg2, psycopg2.extras
 import pymysql
 import json
-from flask import Flask, render_template, request, url_for, redirect, send_file
+from flask import Flask, render_template, request, url_for, redirect, send_file, session
 from collections import OrderedDict 
 import os
 import urllib
 from DataTable import DataTable
 import csv
+from datetime import timedelta
 
 
 app = Flask(__name__)
+
+app.secret_key = os.environ.get('SECRET_KEY')
+
+app.permanent_session_lifetime = timedelta(days=365)
 
 app.jinja_env.filters['urlencode'] = lambda u: urllib.quote(u)
 
 db_uri = os.environ.get("DATABASE_URL")
 
-@app.route('/', methods=["GET"])
+@app.route('/', methods=["GET", "POST"])
 def index():
-	if request.args.get('sql'):
-		sql = request.args.get('sql')
-		sql = urllib.unquote(sql)
-		x = DataTable(db_uri)
-		x.makeTable(sql, css_id="first_test_table", width=8)
-		return render_template('base.html', table1=x, sql=sql)
-	else:	
-		x = DataTable(db_uri)
-		sql = "SELECT customer_id, first_name FROM customer LIMIT 5;"
-		x.makeTable(sql, css_id="first_test_table", width=8)
-		return render_template('base.html', table1=x, sql=sql)
+	if request.method == 'GET':
+		if 'Authenticated' in session.keys() and session['Authenticated'] == True:
+			if request.args.get('sql'):
+				sql = request.args.get('sql')
+				sql = urllib.unquote(sql)
+				x = DataTable(db_uri)
+				x.makeTable(sql, css_id="first_test_table", width=8)
+				return render_template('base.html', table1=x, sql=sql)
+			else:	
+				x = DataTable(db_uri)
+				sql = "SELECT customer_id, first_name FROM customer LIMIT 5;"
+				x.makeTable(sql, css_id="first_test_table", width=8)
+				return render_template('base.html', table1=x, sql=sql)
+		else:
+			return render_template('login.html')
+	if request.method == 'POST':
+		passphrase = request.form.get('passphrase')
+		if passphrase == 'lockpick':
+			session['Authenticated'] = True
+			session.permanent = True
+			return redirect(url_for('index'))
+		else:
+			return render_template('login.html', error=True)
 
 @app.route('/download_csv', methods=['GET'])
 def download_csv():
